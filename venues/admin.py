806c0f6e23ffaf18cb4d0cf6e16bb5f7ccb46725
tsevre <tsevrentzis75@gmail.com>
2025-08-02 11:19:24 +0300

    Fix admin venue permissions
    Restrict perimissions of a venue owner to only see and edit his own venue

--- venues/admin.py
+++ venues/admin.py
@@ -1,55 +1,59 @@
-from    django.contrib              import admin
-from    .models                     import Venue, Table, Reservation, VenueApplication, VenueVisit
-from    django.utils.html           import format_html
-from    django.urls                 import path, reverse
-from    django.template.response    import TemplateResponse
-from    django.contrib.admin        import AdminSite
-from    django.utils.html           import format_html
-from    django.contrib.auth.decorators import user_passes_test
+from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from .models import Venue, Table, Reservation, VenueApplication, VenueVisit
 
-class TableInline(admin.TabularInline):  # or StackedInline if you prefer
+
+class TableInline(admin.TabularInline):
     model = Table
-    extra = 1  # How many blank inlines to show
+    extra = 1
+
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
-    readonly_fields     = ['image_tag']
-    inlines             = [TableInline]  # your existing inline
+    readonly_fields = ['image_tag', 'owner']
+    inlines = [TableInline]
 
-    @admin.display(description="Venue Dashboard")
-    def open_dashboard(self, obj):
-        try:
-            url = reverse('venue_dashboard', kwargs={'venue_id': obj.id})
-        except Exception as e:
-            url = '#'
-        print(url)
-        return format_html('<a href="{}" target="_blank">Open Dashboard</a>', url)
-    # dashboard_link.short_description = "Venue Dashboard"
-
+    list_display = ('name', 'kind', 'location', 'available_tables', 'average_rating', 'image_tag', 'open_dashboard')
+    
     def image_tag(self, obj):
         if obj.image:
             return format_html('<img src="{}" style="height: 50px;" />', obj.image.url)
         return "No Image"
     image_tag.short_description = 'Image'
 
-    list_display        = ('name', 'kind', 'location', 'available_tables', 'average_rating', 'image_tag', 'open_dashboard',)
-    
-    def save_model(self, request, obj, form, change):
-        created = not change
-        super().save_model(request, obj, form, change)
-        # Your existing code for auto-creating owner user, etc.
+    @admin.display(description="Venue Dashboard")
+    def open_dashboard(self, obj):
+        try:
+            url = reverse('venue_dashboard', kwargs={'venue_id': obj.id})
+            return format_html('<a href="{}" target="_blank">Open Dashboard</a>', url)
+        except Exception:
+            return "Unavailable"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser  # Venue admins can't add venues
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return obj is None or obj.owner == request.user
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return obj is not None and obj.owner == request.user
 
     def get_model_perms(self, request):
         perms = super().get_model_perms(request)
         if request.user.is_superuser or request.user.user_type == 'venue_admin':
             return perms
-        return {}  # Hide model completely for other users
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.user_type == 'venue_admin':
-            return qs.filter(owner=request.user)  # Use 'owner' field from your model
-        return qs
+        return {}
 
 
 @admin.register(Table)
@@ -57,7 +61,6 @@ class TableAdmin(admin.ModelAdmin):
     list_display = ('venue', 'number', 'seats')
     list_filter = ('venue',)
     search_fields = ('venue__name',)
-    # autocomplete_fields = ['venue']
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -75,14 +78,17 @@ class TableAdmin(admin.ModelAdmin):
             return False
         return super().has_delete_permission(request, obj)
 
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        # Allow adding if user owns at least one venue
+        return Venue.objects.filter(owner=request.user).exists()
+
     def get_model_perms(self, request):
         perms = super().get_model_perms(request)
         if request.user.is_superuser or request.user.user_type == 'venue_admin':
             return perms
-        return {}  # Hide this model completely from dashboard for other users
-
-
-
+        return {}
 
 
 @admin.register(Reservation)
@@ -94,29 +100,28 @@ class ReservationAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        # Superusers see all reservations
         if request.user.is_superuser:
             return qs
-        # Venue owners only see reservations for their venues
         return qs.filter(venue__owner=request.user)
 
     def has_change_permission(self, request, obj=None):
-        # Prevent venue admins from editing others' reservations
         if not request.user.is_superuser and obj and obj.venue.owner != request.user:
             return False
         return super().has_change_permission(request, obj)
 
     def has_delete_permission(self, request, obj=None):
-        # Same logic for deletion
         if not request.user.is_superuser and obj and obj.venue.owner != request.user:
             return False
         return super().has_delete_permission(request, obj)
-    
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser or Venue.objects.filter(owner=request.user).exists()
+
     def get_model_perms(self, request):
         perms = super().get_model_perms(request)
         if request.user.is_superuser or request.user.user_type == 'venue_admin':
             return perms
-        return {}  # Hide this model completely from dashboard for other users
+        return {}
 
 
 @admin.register(VenueApplication)
@@ -132,6 +137,12 @@ class VenueApplicationAdmin(admin.ModelAdmin):
         updated = queryset.update(reviewed=True, accepted=True)
         self.message_user(request, f"{updated} application(s) marked as accepted.")
 
+    def get_model_perms(self, request):
+        # Only superusers should manage applications
+        if request.user.is_superuser:
+            return super().get_model_perms(request)
+        return {}
+
 
 @admin.register(VenueVisit)
 class VenueVisitAdmin(admin.ModelAdmin):
@@ -140,3 +151,13 @@ class VenueVisitAdmin(admin.ModelAdmin):
     search_fields = ('venue__name', 'user__username', 'ip_address')
     readonly_fields = ('venue', 'user', 'ip_address', 'timestamp')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(venue__owner=request.user)
+
+    def get_model_perms(self, request):
+        if request.user.is_superuser or request.user.user_type == 'venue_admin':
+            return super().get_model_perms(request)
+        return {}
